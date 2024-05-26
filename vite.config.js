3c9565d1fcea82f8fa17231287f00422f9fef5e6
Aeon Merx <103615875+aeonmerx@users.noreply.github.com>
2024-05-26 15:25:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+     build: {
+    outDir: 'public' // Cambia esto al directorio público que estás utilizando en Laravel
+  },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
